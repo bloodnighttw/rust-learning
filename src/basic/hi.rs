9ee58106bf2fb0a9d6dd3f3446f47c0cf3997243
bloodnighttw <bbeenn1227@gmail.com>
2023-12-04 23:17:29 +0800

    init

---
+++ src/basic/hi.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod hi_from_mod {
+    #[test]
+    fn it_works() {
+        println!("hi");
+    }
+}
---
+++ src/hello_world.rs
@@ -0,0 +1,7 @@
+#[cfg(test)]
+mod hello_world {
+    #[test]
+    fn run() {
+        println!("hello world");
+    }
+}
